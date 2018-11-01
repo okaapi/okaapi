06e60f3f5579b6c59fce6fba2e5dc09386dc46c1
root <root@ip-172-31-7-58.us-west-2.compute.internal>
2018-11-01 14:56:06 -0700

    minor change to Gemfile (loofah)

--- lib/https_test.rb
+++ lib/https_test.rb
@@ -4,12 +4,31 @@ require 'json'
 
 def try_https( url )
   uri = URI( 'https://'+url+'/index' )
+  puts "trying URI https://"+url+"/index"
   http = Net::HTTP::new( uri.host, uri.port)
   http.use_ssl = true
   request = Net::HTTP::Get.new(uri.request_uri)
   response = http.request(request)
-  p response
+  return response
 
 end
 
-try_https('www.menhardt.com')
+def try_http( url )
+  uri = URI( 'http://'+url+'/index' )
+  puts "trying URI http://"+url+"/index"
+  http = Net::HTTP::new( uri.host, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.request(request)
+  return response
+
+end
+
+#p try_https('www.menhardt.com')
+#p try_http('www.menhardt.com')
+
+uri = URI( 'https://www.google.com' )
+puts "trying URI https://www.google.com"
+http = Net::HTTP::new( uri.host, uri.port)
+http.use_ssl = true
+request = Net::HTTP::Get.new(uri.request_uri)
+response = http.request(request)
