0a1ceefb69d4b853dd0902ad4354ca9329bda8aa
Wido <you@example.com>
2019-03-16 05:08:20 -0700

    d

--- lib/okaapi_background.rb
+++ lib/okaapi_background.rb
@@ -19,7 +19,7 @@ puts
 puts "SENDING MAILS"
 puts 
 
-if ( Time.now.min < 15 ) && ( Time.now.hour == 18 )
+if ( Time.now.hour == 18 )
   n = Postoffice.send_all_diary_emails( 1959 ) 
   puts "sent #{n} diary reminder emails"
   n = Postoffice.send_okaapi_emails 
